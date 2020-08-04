aec11b9d29606a5fc55bb7eff0075af55caa58e4
Michael Grier <michael.c.grier@gmail.com>
2020-08-04 08:37:56 -0600

    finish examinee/create (registration) and fix ExamineeContext

---
+++ Invigulus.App/Controllers/ExamineeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Invigulus.Business;
+using Invigulus.Data.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invigulus.App.Controllers
+{
+    public class ExamineeController : Controller
+    {
+        // generate view to add new examinee
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // add new asset to database
+        [HttpPost]
+        public IActionResult Create(Examinee examinee)
+        {
+            // if new asset type is successfully added to db, redirect to login page. if not, reload current view
+            try
+            {
+                ExamineeManager.Add(examinee);
+                Console.WriteLine("success" + examinee);
+                return RedirectToAction("Home");
+            }
+            catch
+            {
+                Console.WriteLine("fail" + examinee);
+                return View();
+            }
+        }
+    }
+}
--- Invigulus.Data/ExamineeContext.cs
+++ Invigulus.Data/ExamineeContext.cs
@@ -4,10 +4,18 @@ using System;
 
 namespace Invigulus.Data
 {
-    public class ExamineeContext : DbContext
+    public class ExamineeContext : InvigulusContext
     {
         public ExamineeContext() : base() { }
 
         public DbSet<Examinee> Examinees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // change this connection string to work on your local machine
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;
+                                          Database=Invigulus;
+                                          Trusted_Connection=True;");
+        }
     }
 }
