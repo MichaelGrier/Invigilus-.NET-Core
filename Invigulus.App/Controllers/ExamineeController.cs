8f1250378e6729d6abe5a7c3c2d953ad1b2b5b47
Chris Lee <chrislee910320@gmail.com>
2020-08-04 16:14:19 -0600

    The Thanks page can put the username from the data that gets from Cookies

--- Invigulus.App/Controllers/ExamineeController.cs
+++ Invigulus.App/Controllers/ExamineeController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Invigulus.Business;
 using Invigulus.Data.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+//using System.Security.Claims.ClaimsPrincipal;
+using System.Security.Principal;
+using System.Threading;
 
 namespace Invigulus.App.Controllers
 {
@@ -36,6 +41,40 @@ namespace Invigulus.App.Controllers
 
         public IActionResult Thanks()
         {
+            //var userId = HttpContext.Request.Cookies.Keys;      /*   context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;*/
+            //httpcookie
+            //var userName = HttpContext.Request.Cookies.ContainsKey("Name");
+
+            //var principal = System.Security.Claims.ClaimsPrincipal.Current;
+            //string fname = principal.FindFirst(ClaimTypes.Name).Value;
+
+            //ViewBag.userName = fname;
+
+
+            ////Get the current claims principal
+            //var identity = (ClaimsPrincipal)Thread.CurrentPrincipal;
+
+            //// Get the claims values
+            //var name = identity.Claims.Where(c => c.Type == ClaimTypes.Name)
+            //                   .Select(c => c.Value).SingleOrDefault();
+            ////var sid = identity.Claims.Where(c => c.Type == ClaimTypes.Sid)
+            ////                   .Select(c => c.Value).SingleOrDefault();
+            ///
+            //var claimsIdentity = (ClaimsIdentity)this.RequestContext.Principal.Identity;
+            //var name = null;
+            //foreach (var claim in claimsIdentity.Claims)
+            //{
+            //    name = claim.Value;
+            //    // claim.value;
+            //    // claim.Type
+            //}
+
+        
+
+            //ViewBag.userName = name;
+
+
+
             return View();
         }
     }
