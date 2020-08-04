452d1b696a589e806250960530b2112bc7bcb90d
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2020-08-04 13:54:40 -0600

    added pages

---
+++ Invigulus.App/Controllers/ExamPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invigulus.App.Controllers
+{
+    public class ExamPageController : Controller
+    {
+        public IActionResult Loading()
+        {
+            return View();
+        }
+
+        public IActionResult Thanks()
+        {
+            return View();
+        }
+    }
+}
