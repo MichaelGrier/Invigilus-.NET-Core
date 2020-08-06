a1509ccaf27ea569212079d6c3f199f5ce43148a
Chris Lee <chrislee910320@gmail.com>
2020-08-05 20:21:48 -0600

    Commit for merge

--- Invigulus.App/Controllers/ExamineeController.cs
+++ Invigulus.App/Controllers/ExamineeController.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Invigulus.Business;
 using Invigulus.Data.Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-//using System.Security.Claims.ClaimsPrincipal;
-using System.Security.Principal;
-using System.Threading;
 
 namespace Invigulus.App.Controllers
 {
@@ -21,16 +17,16 @@ namespace Invigulus.App.Controllers
             return View();
         }
 
-        // add new asset to database
+        // add new examinee to database
         [HttpPost]
         public IActionResult Create(Examinee examinee)
         {
-            // if new asset type is successfully added to db, redirect to login page. if not, reload current view
+            // if new examinee is successfully added to db, redirect to login page. if not, reload current view
             try
             {
                 ExamineeManager.Add(examinee);
                 Console.WriteLine("success" + examinee);
-                return RedirectToAction("Index", "UserAuth");
+                return RedirectToAction("Login", "Account");
             }
             catch
             {
@@ -39,42 +35,41 @@ namespace Invigulus.App.Controllers
             }
         }
 
+        [Authorize]
+        public IActionResult Display()
+        {
+            var examinee = ExamineeManager.GetLast();
+            return View(examinee);
+        }
+
+        [Authorize]
+        // generate view to edit examinee info
+        public ActionResult Edit(int id)
+        {
+            var examinee = ExamineeManager.Find(id);
+            return View(examinee);
+        }
+
+        // edit examinee info
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Examinee examinee)
+        {
+            try
+            {
+                // call the ExamineeManager to edit
+                ExamineeManager.Update(examinee);
+
+                return RedirectToAction("Display");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
         public IActionResult Thanks()
         {
-            //var userId = HttpContext.Request.Cookies.Keys;      /*   context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;*/
-            //httpcookie
-            //var userName = HttpContext.Request.Cookies.ContainsKey("Name");
-
-            //var principal = System.Security.Claims.ClaimsPrincipal.Current;
-            //string fname = principal.FindFirst(ClaimTypes.Name).Value;
-
-            //ViewBag.userName = fname;
-
-
-            ////Get the current claims principal
-            //var identity = (ClaimsPrincipal)Thread.CurrentPrincipal;
-
-            //// Get the claims values
-            //var name = identity.Claims.Where(c => c.Type == ClaimTypes.Name)
-            //                   .Select(c => c.Value).SingleOrDefault();
-            ////var sid = identity.Claims.Where(c => c.Type == ClaimTypes.Sid)
-            ////                   .Select(c => c.Value).SingleOrDefault();
-            ///
-            //var claimsIdentity = (ClaimsIdentity)this.RequestContext.Principal.Identity;
-            //var name = null;
-            //foreach (var claim in claimsIdentity.Claims)
-            //{
-            //    name = claim.Value;
-            //    // claim.value;
-            //    // claim.Type
-            //}
-
-        
-
-            //ViewBag.userName = name;
-
-
-
             return View();
         }
     }
